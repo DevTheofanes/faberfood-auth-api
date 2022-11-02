83fdc9038973366f920d42fd45cd6765a9a35304
DevTheofanes <devtheofanes@gmail.com>
2022-11-02 16:03:23 -0300

    feat: domain layer to user service

---
+++ src/domain/services/user.ts
@@ -0,0 +1,22 @@
+import { Prisma } from '@prisma/client';
+
+export interface IUserParams {
+  name: string;
+  username: string;
+  email?: string;
+  phone?: string;
+  password: string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string | null;
+  phone: string | null;
+  password: string;
+  active: boolean;
+  userAccess: Prisma.JsonValue;
+  createdAt: Date;
+  updatedAt: Date;
+}
